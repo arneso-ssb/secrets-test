e8ba3ce47f2347704d7bd95a0bdd207f349a6858
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2022-08-25 13:53:43 +0200

    Add a small example using python-dotenv module

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # secrets-test
 Repo for testing handling of secretes i Python, R and with GitHub Actions.
+
+## Usage
+Create a `.env` file in the same directory as the secrets-test.py script and
+with some content like:
+```dotenv
+SECRET_KEY=v%)9n7kg^65(4-uir6!pa@oqqsdn8agv9h8
+DOMAIN=tester.org
+EMAIL=admin@${DOMAIN}
+```
+
+Run the script:
+```shell
+secrets-test.py
+```
---
+++ secrets-test.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()  # take environment variables from .env.
+
+# retrieving keys and adding them to the project
+# from the .env file through their key names
+SECRET_KEY = os.getenv("SECRET_KEY")
+DOMAIN = os.getenv("DOMAIN")
+EMAIL = os.getenv("EMAIL")
+
+print(f"{SECRET_KEY = }")
+print(f"{DOMAIN = }")
+print(f"{EMAIL = }")
